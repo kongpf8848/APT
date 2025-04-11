05a73506f5e55befc9bd4c12d4fdd410daa9c7d7
kongpf8848 <kongpf8848@gmail.com>
2025-04-11 18:34:07 +0800

    first commit

---
+++ processor/src/main/java/org/example/processor/AdderProcessor.java
@@ -0,0 +1,74 @@
+package org.example.processor;
+
+import com.google.auto.service.AutoService;
+import com.squareup.javapoet.ClassName;
+import com.squareup.javapoet.JavaFile;
+import com.squareup.javapoet.MethodSpec;
+import com.squareup.javapoet.TypeSpec;
+import org.example.annotation.Adder;
+
+import javax.annotation.processing.*;
+import javax.lang.model.SourceVersion;
+import javax.lang.model.element.*;
+import javax.tools.Diagnostic;
+import java.io.IOException;
+import java.util.Set;
+
+@AutoService(Processor.class)
+@SupportedAnnotationTypes("org.example.annotation.Adder")
+@SupportedSourceVersion(SourceVersion.RELEASE_17)
+public class AdderProcessor extends AbstractProcessor {
+
+    @Override
+    public boolean process(Set<? extends TypeElement> annotations, RoundEnvironment roundEnv) {
+        System.out.println("++++++++++++AdderProcessor process++++++++++");
+        for (Element element : roundEnv.getElementsAnnotatedWith(Adder.class)) {
+            if (element.getKind() != ElementKind.METHOD) {
+                continue;
+            }
+
+            ExecutableElement method = (ExecutableElement) element;
+            Adder adder = method.getAnnotation(Adder.class);
+
+            int num1 = adder.num1();
+            int num2 = adder.num2();
+            System.out.println("++++++++++++AdderProcessor num1:" + num1 + ",num2:" + num2);
+            String className = ((TypeElement) method.getEnclosingElement()).getQualifiedName().toString();
+            System.out.println("++++++++++++AdderProcessor className:" + className + ",methodName:" + method.getKind());
+            generateCode(className + "Gen", method.getSimpleName().toString(), num1, num2);
+        }
+        return true;
+    }
+
+    private void generateCode(String className, String methodName, int a, int b) {
+        try {
+            MethodSpec methodSpec = MethodSpec.methodBuilder(methodName)
+                    .addModifiers(Modifier.PUBLIC, Modifier.STATIC)
+                    .returns(void.class)
+                    .addStatement("$T.out.println(\"Result: $L\")", System.class, (a + b))
+                    .build();
+
+            ClassName clazz = ClassName.get(getPackage(className), getSimpleClassName(className));
+            TypeSpec typeSpec = TypeSpec.classBuilder(clazz)
+                    .addMethod(methodSpec)
+                    .build();
+            JavaFile javaFile = JavaFile.builder(getPackage(className), typeSpec)
+                    .addFileComment("made by jack\n")
+                    .addFileComment("This codes are generated automatically. Do not modify!")
+                    .build();
+            javaFile.writeTo(processingEnv.getFiler());
+
+        } catch (IOException e) {
+            processingEnv.getMessager().printMessage(Diagnostic.Kind.ERROR, e.getMessage());
+        }
+    }
+
+    private String getPackage(String className) {
+        return className.contains(".") ? className.substring(0, className.lastIndexOf('.')) : "";
+    }
+
+    private String getSimpleClassName(String className) {
+        return className.contains(".") ?
+                className.substring(className.lastIndexOf('.') + 1) : className;
+    }
+}
---
+++ sample/src/main/java/org/example/Main.java
@@ -0,0 +1,17 @@
+package org.example;
+
+import org.example.annotation.Adder;
+import org.example.MainGen;
+
+public class Main {
+
+    @Adder(num1 = 17, num2 = 25)
+    public static void calculateSum() {
+
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+        MainGen.calculateSum();
+    }
+}
